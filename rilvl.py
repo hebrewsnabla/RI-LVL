7df82d07170be8fa8b1e0caec05d093a4613d573
hebrewsnabla <srwang20@fudan.edu.cn>
2022-07-11 16:19:24 +0800

    test eri

--- rilvl.py
+++ rilvl.py
@@ -1,13 +1,28 @@
-from pyscf import gto
+from pyscf import gto, lib
 from pyscf.gto.mole import PTR_ENV_START #_rm_digit, NUC_POINT, ATOM_OF, ATM_SLOTS, BAS_SLOTS, _parse_nuc_mod, make_atm_env, make_bas_env
 from pyscf.gto.moleintor import getints
 import sys
 import numpy as np
 
+def nao_by_atom(mol):
+    nao_lst = []
+    for a, (s1, s2, b1, b2) in enumerate(mol.aoslice_by_atom()):
+        nao_lst.append((s2-s1,b2-b1))
+    return nao_lst
+
 def make_env_IJ(mol):
     #print(mol._atom)
     #print(mol._basis)
+    #nao_lst = nao_by_atom(mol)
+    aoslice = mol.aoslice_by_atom()
     abe = []
+    for I in range(mol.natm):
+        fake_atom = [mol._atom[I]]
+        fake_basis = mol._basis
+        _atm, _bas, _env = gto.mole.make_env(fake_atom, fake_basis, 
+                    np.zeros(PTR_ENV_START), mol.nucmod, mol.nucprop)
+        abe.append([_atm, _bas, _env, aoslice[I], None])
+
     for I in range(mol.natm):
         for J in range(I+1, mol.natm):
             fake_atom = [mol._atom[I], mol._atom[J]]
@@ -17,7 +32,7 @@ def make_env_IJ(mol):
             #print(_atm)
             #print(_bas)
             #print(_env)
-            abe.append([_atm, _bas, _env])
+            abe.append([_atm, _bas, _env, aoslice[I], aoslice[J]])
     return abe
 
 
@@ -25,18 +40,134 @@ def riints(mol, auxmol):
     abe = make_env_IJ(mol)
     abe_aux = make_env_IJ(auxmol)
     
-    intor = mol._add_suffix('int3c2e')
+    intor3c2e = mol._add_suffix('int3c2e')
+    intor2c2e = mol._add_suffix('int2c2e')
     #hermi = 0
     #ao_loc = None
     #aoslice = mol.aoslice_by_atom()
     #aoslice_aux = auxmol.aoslice_by_atom()
-    for IJ in range(len(abe)):
-        atm_IJ, bas_IJ, env_IJ = abe[IJ]
-        atmx_IJ, basx_IJ, envx_IJ = abe_aux[IJ]
+    ints_IJ = []
+    for I in range(mol.natm):
+        atm_I, bas_I, env_I, aosl_I, _ = abe[I]
+        atmx_I, basx_I, envx_I, _, _ = abe_aux[I]
+        atm, bas, env = gto.mole.conc_env(atm_I, bas_I, env_I,
+                                      atmx_I, basx_I, envx_I)
+        nbas = bas_I.shape[0]
+        nauxbas = basx_I.shape[0]
+        shls_slice = (0, nbas, 0, nbas, nbas, nbas+nauxbas)
+        ints = getints(intor3c2e, atm, bas, env, shls_slice)
+        V = getints(intor2c2e, atmx_I, basx_I, envx_I, (0, nauxbas, 0, nauxbas))
+        C = lib.einsum('ijP, PQ -> ijQ', ints, np.linalg.inv(V))
+        aosl = slice(aosl_I[2], aosl_I[3])
+        ints_IJ.append([C, aosl, aosl])
+        print('Pair %d'%I, 'RI ints shape', ints.shape, 'V shape', V.shape)
+    for IJ in range(mol.natm, len(abe)):
+        atm_IJ, bas_IJ, env_IJ, aosl_I, aosl_J = abe[IJ]
+        atmx_IJ, basx_IJ, envx_IJ, _, _ = abe_aux[IJ]
         atm, bas, env = gto.mole.conc_env(atm_IJ, bas_IJ, env_IJ,
                                       atmx_IJ, basx_IJ, envx_IJ)
         nbas = bas_IJ.shape[0]
         nauxbas = basx_IJ.shape[0]
-        shls_slice = (0, nbas, 0, nbas, nbas, nbas+nauxbas)
-        ints = getints(intor, atm, bas, env, shls_slice)
-        print('Pair %d'%IJ, 'RI ints shape', ints.shape)
+        nshl_I = aosl_I[1] - aosl_I[0]
+        shls_slice = (0, nshl_I, nshl_I, nbas, nbas, nbas+nauxbas)
+        ints = getints(intor3c2e, atm, bas, env, shls_slice)
+        V = getints(intor2c2e, atmx_IJ, basx_IJ, envx_IJ, (0, nauxbas, 0, nauxbas))
+        C = lib.einsum('ijP, PQ -> ijQ', ints, np.linalg.inv(V))
+        aosls_I = slice(aosl_I[2], aosl_I[3])
+        aosls_J = slice(aosl_J[2], aosl_J[3])
+        ints_IJ.append([C, aosls_I, aosls_J])
+        print('Pair %d'%IJ, 'RI ints shape', ints.shape, 'V shape', V.shape)
+    return ints_IJ
+
+def riints2(auxmol):
+    abe_aux = make_env_IJ(auxmol)
+    intor2c2e = auxmol._add_suffix('int2c2e')
+    V2 = []
+    for IJ in range(len(abe_aux)):
+        V2_IJ = []
+        atmx_IJ, basx_IJ, envx_IJ, _, _ = abe_aux[IJ]
+        nbas_IJ = basx_IJ.shape[0]
+        #for KL in range(IJ, len(abe_aux)):
+        for KL in range(len(abe_aux)):
+            atmx_KL, basx_KL, envx_KL, _, _ = abe_aux[KL]
+            nbas_KL = basx_KL.shape[0]
+            atm, bas, env = gto.mole.conc_env(atmx_IJ, basx_IJ, envx_IJ,
+                                      atmx_KL, basx_KL, envx_KL)
+            shls_slice = (0, nbas_IJ, nbas_IJ, nbas_IJ + nbas_KL)
+            ints = getints(intor2c2e, atm, bas, env, shls_slice)
+            print('Pair %d-%d'%(IJ,KL), '(P|Q) ints shape', ints.shape)
+            V2_IJ.append(ints)
+        V2.append(V2_IJ)
+    return V2
+
+def get_eri(mol, auxmol):
+    ints_3c = riints(mol, auxmol)
+    ints_V = riints2(auxmol)
+    nao = nmo = mol.nao
+    eri = np.zeros((nao, nao, nao, nao))
+    size = 0
+    for IJ in range(len(ints_3c)):
+        int_3c_IJ, aosl1, aosl2 = ints_3c[IJ]
+        #if IJ < mol.natm:
+        #    fac1 = 1.0
+        #else:
+        #    fac1 = 2.0
+        #for KL in range(IJ, len(ints_3c)):
+        for KL in range(len(ints_3c)):
+            #print('%d-%d'%(IJ,KL))
+            int_3c_KL, aosl3, aosl4 = ints_3c[KL]
+            int_V = ints_V[IJ][KL]
+            #print(int_3c_IJ.shape, int_V.shape, int_3c_KL.shape)
+            #if KL < mol.natm:
+            #    fac2 = 1.0
+            #else:
+            #    fac2 = 2.0
+            ints = lib.einsum("uvP, PQ, klQ -> uvkl", int_3c_IJ, int_V, int_3c_KL)
+            eri[aosl1, aosl2, aosl3, aosl4] = ints
+            print(IJ, KL, aosl1, aosl2, aosl3, aosl4)
+            s = ints.size
+            size += s
+            if aosl1 != aosl2:
+                eri[aosl2, aosl1, aosl3, aosl4] = ints.transpose(1,0,2,3)
+                size += s
+            if aosl3 != aosl4:
+                eri[aosl1, aosl2, aosl4, aosl3] = ints.transpose(0,1,3,2)
+                #print('0132')
+                size += s
+            if aosl1 != aosl2 and aosl3 != aosl4:
+                eri[aosl2, aosl1, aosl4, aosl3] = ints.transpose(1,0,3,2)
+                #print('1032')
+                size += s
+    print(size, nao**4)
+    return eri
+
+
+def rimp2(mol, auxmol, mo_coeff):
+    ints_3c = riints(mol, auxmol)
+    ints_V = riints2(auxmol)
+    nao = nmo = mol.nao
+    nocc = mol.nelec[0]
+    nvir = nmo - nocc
+    so, sv, sa = slice(0, nocc), slice(nocc, nmo), slice(0, nmo)
+    Co, Cv = mo_coeff[:, so], mo_coeff[:, sv]
+    int_iajb = np.zeros((nocc, nvir, nocc, nvir))
+    for IJ in range(len(ints_3c)):
+        int_3c_IJ, aosl1, aosl2 = ints_3c[IJ]
+        if IJ < mol.natm:
+            fac1 = 1.0
+        else:
+            fac1 = 2.0
+        #for KL in range(IJ, len(ints_3c)):
+        for KL in range(len(ints_3c)):
+            print('%d-%d'%(IJ,KL))
+            int_3c_KL, aosl3, aosl4 = ints_3c[KL]
+            int_V = ints_V[IJ][KL]
+            #print(int_3c_IJ.shape, int_V.shape, int_3c_KL.shape)
+            if KL < mol.natm:
+                fac2 = 1.0
+            else:
+                fac2 = 2.0
+            int_iajb += fac1*fac2*lib.einsum("ui, va, uvP, PQ, klQ, kj, lb -> iajb", 
+                    Co[aosl1,:], Cv[aosl2,:], int_3c_IJ, int_V, int_3c_KL, 
+                    Co[aosl3,:], Cv[aosl4,:])
+    return int_iajb
--- test.py
+++ test.py
@@ -1,5 +1,8 @@
-from pyscf import gto, scf
+from pyscf import gto, scf, mp, lib, df
 import rilvl
+import scipy
+import numpy as np
+lib.num_threads(1)
 
 mol = gto.Mole()
 mol.atom = """
@@ -16,7 +19,77 @@ print(mol.aoslice_by_atom())
 auxmol = mol.copy()
 auxmol.basis = "def2-tzvp-jkfit"
 auxmol.build()
-print(auxmol.aoslice_by_atom())
+#print(auxmol.aoslice_by_atom())
 
-#rilvl.make_env_IJ(mol)
-rilvl.riints(mol, auxmol)
+scf_df = scf.RHF(mol).density_fit().run()
+mo_coeff = scf_df.mo_coeff
+#mp2_df = mp.MP2(scf_df).run()
+
+nao = nmo = mol.nao
+nocc = mol.nelec[0]
+nvir = nmo - nocc
+so, sv, sa = slice(0, nocc), slice(nocc, nmo), slice(0, nmo)
+Co, Cv = mo_coeff[:, so], mo_coeff[:, sv]
+int2c2e = auxmol.intor("int2c2e")
+int3c2e = df.incore.aux_e2(mol, auxmol)
+nao_df = auxmol.nao
+#int2c2e_half = scipy.linalg.cholesky(int2c2e, lower=True)
+#V_df_mp2 = scipy.linalg.solve_triangular(int2c2e_half, int3c2e.reshape(-1, nao_df).T, lower=True)\
+#               .reshape(nao_df, nao, nao).transpose((1, 2, 0))
+#eri_iajb = lib.einsum("ui, va, uvP, klP, kj, lb -> iajb", Co, Cv, V_df_mp2, V_df_mp2, Co, Cv)
+
+#V_df = tril_to_symm(scf_df.with_df._cderi).transpose((1, 2, 0))
+df_eri = lib.einsum("uvP, PQ, klQ -> uvkl", int3c2e, np.linalg.inv(int2c2e), int3c2e)
+eri = mol.intor("int2e")
+#rilvl.riints2(auxmol)
+#int_iajb = rilvl.rimp2(mol, auxmol, mo_coeff)
+#print(eri_iajb[0,0,0,0], int_iajb[0,0,0,0])
+auxmol = mol.copy()
+auxmol.basis = "def2-tzvp-jkfit"
+auxmol.build()
+lvl_eri = rilvl.get_eri(mol, auxmol)
+print(np.allclose(lvl_eri, eri, 0.001))
+print((lvl_eri-eri).max())
+
+print('i, j, k, l, RI-LVL, RI-V')
+err2=0
+err3=0
+for i in range(nao):
+    for j in range(nao):
+        for k in range(nao):
+            for l in range(nao):
+                dd = lvl_eri[i,j,k,l] - eri[i,j,k,l]
+                if abs(dd) > 1e-2:
+                    #print(i,j,k,l,eri[i,j,k,l], df_eri[i,j,k,l])
+                    err2+=1
+                if abs(dd) > 1e-3:
+                    err3 += 1
+print('err num', err2, err3)
+
+import matplotlib.pyplot as plt
+bins =np.arange(-12, 1., 0.1)
+histo_eri = np.histogram(np.log10(np.abs(eri).ravel() + 1e-20), bins=bins)
+histo_df_eri = np.histogram(np.log10(np.abs(df_eri).ravel() + 1e-20), bins=bins)
+histo_lvl_eri = np.histogram(np.log10(np.abs(lvl_eri).ravel() + 1e-20), bins=bins)
+histo_dev0 = np.histogram(np.log10(np.abs(df_eri - eri).ravel() + 1e-20), bins=bins)
+histo_dev = np.histogram(np.log10(np.abs(lvl_eri - eri).ravel() + 1e-20), bins=bins)
+
+prob_eri = histo_eri[0] / eri.size
+prob_df_eri = histo_df_eri[0] / eri.size
+prob_lvl_eri = histo_lvl_eri[0] / eri.size
+prob_dev0 = histo_dev0[0] / eri.size
+prob_dev = histo_dev[0] / eri.size
+
+fig, ax = plt.subplots()
+
+ax.plot(histo_eri[1][:-1], prob_eri, label="noRI AO ERI", linestyle="-.")
+ax.plot(histo_df_eri[1][:-1], prob_df_eri, label="RI-V AO ERI", linestyle=":")
+ax.plot(histo_df_eri[1][:-1], prob_lvl_eri, label="RI-LVL AO ERI", linestyle=":")
+ax.plot(histo_dev0[1][:-1], prob_dev0, label="Deviation RI-V vs noRI")
+ax.plot(histo_dev[1][:-1], prob_dev, label="Deviation RI-LVL vs noRI")
+
+ax.set_xlabel("$\mathrm{log}_{10} \mathrm{AO\, ERI}$")
+ax.set_ylabel("Probability Distribution")
+ax.set_title("AO ERI Distribution Status")
+ax.legend()
+fig.savefig('test.png')
